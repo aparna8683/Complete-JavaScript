2b8513ece3363a0e6a54ebbaedc162d88f0cbfc2
Aparna Singh <aparnasingh868383@gmail.com>
2026-06-26 14:08:07 +0530

    aaded first commit

--- newJavaScript/index.js
+++ newJavaScript/index.js
@@ -2,11 +2,59 @@ const taskInput= document.getElementById("todo-text")
 const addButton=document.getElementById("add-todo")
 const todoList=document.getElementById("todo-list")
 let todos=[]
+function saveTodos(){
+    localStorage.setItem("todos", JSON.stringify(todos))
+}
+function getTodos(){
+    const savedTodos=localStorage.getItem("todos")
+    if(savedTodos){
+        return JSON.parse(savedTodos)
+    }
+    return []
+}
+
+function deleteTodo(index){
+    todos.splice(index, 1)
+    saveTodos();
+    renderTodos()
+}  
+
+function toggleTodo(index){
+    todos[index].isCompleted=!todos[index].isCompleted
+    saveTodos();
+    renderTodos()
+} 
+todos=getTodos()
+renderTodos()
+let editIndex=null
+
+function editTodo(index){
+    editIndex=index
+    addButton.textContent="Update Todo"
+    taskInput.value=todos[index].text
+    
+}
+
+
 function renderTodos(){
     todoList.innerHTML=""
-    todos.forEach(function(todo){
+    
+    todos.forEach((todo, index)=>{
         const li=document.createElement("li")
-        li.innerText=todo
+                let displaytext
+        if(todo.isCompleted){
+            displaytext="✓ "
+    }
+            else{
+                displaytext=""
+            }
+        li.innerHTML=`${displaytext}${todo.text} <button onclick="deleteTodo(${index})">Delete</button> 
+
+        <button onclick="toggleTodo(${index})">Completed?
+        
+        </button>
+        <button onclick="editTodo(${index})">Edit Todo</button>`
+        
         todoList.appendChild(li)
 
     })
@@ -14,9 +62,21 @@ function renderTodos(){
 
 }
 addButton.addEventListener("click",function(){
-    const taskValue=taskInput.value
-    todos.push(taskValue)
-    console.log(todos)
+    const taskValue=taskInput.value.trim()
+    if(taskValue===""){
+        return
+    }
+    if(editIndex!==null){
+        todos[editIndex].text=taskValue
+        editIndex=null
+        addButton.textContent="Add Todo"
+    } else {
+        todos.push({
+            text: taskValue,
+            isCompleted: false
+        })
+    }
     taskInput.value=""
+    saveTodos()
     renderTodos()
 })
